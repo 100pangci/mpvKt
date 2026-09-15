e2e723c3cb4199208af87edcdc3a4d947f1c2bac
ywpc05 <ywpc05@gmail.com>
2026-09-15 23:48:00 +0800

    chore(release): bump version to 0.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
     applicationId = "live.ywpc05.mpvkt"
     minSdk = 23
     targetSdk = 36
-    versionCode = 15
-    versionName = "0.2.2"
+    versionCode = 16
+    versionName = "0.2.3"
 
     vectorDrawables {
       useSupportLibrary = true
